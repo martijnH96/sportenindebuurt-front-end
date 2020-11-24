16f76db711aa152c245ef867aebec08e7a30564c
larsb <larsboxmeer@gmail.com>
2020-11-24 13:41:02 +0100

    meerdere formfields gemaakt met goede achtergrond tekst:
    TO-DO: gebruikersinput verwerken en wegschrijven.
    TO-DO: dropdown maken van het "sport" field

--- lib/EventForm.dart
+++ lib/EventForm.dart
@@ -1,17 +1,40 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+void main() => runApp(EventForm());
+
+class EventForm extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final appTitle = 'Form Validation Demo';
+
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(appTitle),
+        ),
+        body: MyCustomForm(),
+      ),
+    );
+  }
+}
+
+// Create a Form widget.
 class MyCustomForm extends StatefulWidget {
   @override
   MyCustomFormState createState() {
     return MyCustomFormState();
   }
 }
+
+// Create a corresponding State class.
+// This class holds data related to the form.
 class MyCustomFormState extends State<MyCustomForm> {
   // Create a global key that uniquely identifies the Form widget
   // and allows validation of the form.
   //
-  // Note: This is a `GlobalKey<FormState>`,
+  // Note: This is a GlobalKey<FormState>,
   // not a GlobalKey<MyCustomFormState>.
   final _formKey = GlobalKey<FormState>();
 
@@ -19,32 +42,80 @@ class MyCustomFormState extends State<MyCustomForm> {
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
     return Form(
-        key: _formKey,
-        child: Column(
-            children: <Widget>[
-        TextFormField(
-        validator: (value) {
-    if (value.isEmpty) {
-    return 'Vul A.U.B. geldige informatie in.';
-    }
-    return null;
-    },
-    ),
-    ElevatedButton(
-      onPressed: () {
-        if (_formKey.currentState.validate()) {
-          Scaffold
-              .of(context)
-              .showSnackBar(SnackBar(content: Text('Processing Data')));
-        }
-      },
-      child: Text('Submit'),
-    )
-            ]
-    )
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          textFormFields,
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 16.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false
+                // otherwise.
+                if (_formKey.currentState.validate()) {
+                  // If the form is valid, display a Snackbar.
+                  Scaffold.of(context)
+                      .showSnackBar(SnackBar(content: Text('Processing Data')));
+                }
+              },
+              child: submitButton,
+
+            ),
+          ),
+        ],
+      ),
     );
   }
 
+  TextFormField buildTextFormField() {
+    return TextFormField(
+          validator: (value) {
+            if (value.isEmpty) {
+              return 'Vul iets in A.U.B.';
+            }
+            return null;
+          },
+        );
+  }
+}
+
+Widget submitButton = Container(
+child: Text('Submit')
+);
+
+Widget textFormFields = Container(
+  // welke textvelden hebben we nodig van de gebruiker?
+  // welke sport, aantal mensen, locatie?, tijd: begin en eind?, speelniveau.
+
+    child: Column (mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildFormTextFields("Sport"),
+        _buildFormTextFields("Aantal deelnemers"),
+        _buildFormTextFields("Locatie"),
+        _buildFormTextFields("Begintijd"),
+        _buildFormTextFields("Eindtijd"),
+        _buildFormTextFields("Speelniveau"),
+      ],
+    )
+);
 
 
+Column _buildFormTextFields( String label) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Container(
+        margin: const EdgeInsets.only(top: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+    ],
+  );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'EventForm.dart';
 import 'homescreen.dart';
 
 void main() {
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: homescreen(),
+      home:EventForm(),
     );
   }
 }
