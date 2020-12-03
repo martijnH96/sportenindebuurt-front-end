1609625ef9b92fd1045df087709903a650e1aa8b
Colin <Colin.t.s@live.com>
2020-12-03 10:46:06 +0100

    materialsneeded toegevoegd

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
-import 'homescreen.dart';
+import 'materialsneeded.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: homescreen(),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: homescreen(),
+//     );
+//   }
+// }
