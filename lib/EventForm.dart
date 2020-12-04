729d144157ddb2de5f436b166e3dea5e9df8918c
martijnH96 <martijnh1996@hotmail.com>
2020-12-04 13:16:34 +0100

    dropdownformfield werkend

--- lib/EventForm.dart
+++ lib/EventForm.dart
@@ -2,12 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:dropdown_formfield/dropdown_formfield.dart';
 
-void main() => runApp(EventForm());
+// void main() => runApp(EventForm()); is nutteloos :D
 
 class EventForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final appTitle = 'Form Validation Demo';
+    final appTitle = 'Event aanmaken';
 
     return MaterialApp(
       title: appTitle,
@@ -24,9 +24,7 @@ class EventForm extends StatelessWidget {
 // Create a Form widget.
 class MyCustomForm extends StatefulWidget {
   @override
-  MyCustomFormState createState() {
-    return MyCustomFormState();
-  }
+  MyCustomFormState createState() => MyCustomFormState();
 }
 
 // Create a corresponding State class.
@@ -45,9 +43,17 @@ class MyCustomFormState extends State<MyCustomForm> {
     return Form(
       key: _formKey,
       child: Column(
+        // welke textvelden hebben we nodig van de gebruiker?
+        // welke sport, aantal mensen, locatie?, tijd: begin en eind?, speelniveau.
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          getTextFormFields(),
+          buildDropDown(), //heb hem er hier maar effe in gedaan
+          _buildFormTextFields("Aantal deelnemers"),
+          _buildFormTextFields("Locatie"),
+          _buildFormTextFields("Begintijd"),
+          _buildFormTextFields("Eindtijd"),
+          _buildFormTextFields("Speelniveau"),
+
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 16.0),
             child: ElevatedButton(
@@ -80,32 +86,13 @@ class MyCustomFormState extends State<MyCustomForm> {
     );
   }
 
-
   Widget submitButton = Container(
       child: Text('Submit')
   );
 
-  Widget getTextFormFields() {
-    return Container(
-      // welke textvelden hebben we nodig van de gebruiker?
-      // welke sport, aantal mensen, locatie?, tijd: begin en eind?, speelniveau.
-
-        child: Column (mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget> [
-            _buildFormTextFields("Sport"),
-            _buildFormTextFields("Aantal deelnemers"),
-            _buildFormTextFields("Locatie"),
-            _buildFormTextFields("Begintijd"),
-            _buildFormTextFields("Eindtijd"),
-            _buildFormTextFields("Speelniveau"),
-          ],
-        )
-    );
-  }
-
   Column _buildFormTextFields(String label) {
     return Column(
-      mainAxisSize: MainAxisSize.min,
+      mainAxisSize: MainAxisSize. min,
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
@@ -118,24 +105,32 @@ class MyCustomFormState extends State<MyCustomForm> {
             ),
           ),
         ),
+        buildTextFormField(),
       ],
     );
   }
+  var _myActivity; //deze staan lokaal - hier kan je dus niks mee in je code naast dat het een opvul plaats is. >>> zet deze buiten de functie
+  String _myActivityResult; // dit ook - Het gaat niet veranderen wanneer je een andere item selecteert. >>>>>>>>>>>^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-  Widget buildDropDown(BuildContext context) {
-    var _myActivity;
-    String _myActivityResult;
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Dropdown Formfield Example'),
-      ),
-      body: Center(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
+
+
+  // Widget buildDropDown(BuildContext context) { //Wanneer wordt deze uberhaupt aangeroepen? Staat nergens in de code. - context hoeft ook niet per se mee
+    Widget buildDropDown(){
+    // var _myActivity; //deze staan lokaal - hier kan je dus niks mee in je code naast dat het een opvul plaats is. >>> zet deze buiten de functie
+    // String _myActivityResult; // dit ook - Het gaat niet veranderen wanneer je een andere item selecteert. >>>>>>>>>>>^^^^^^^^^^^^^^^^^^^^^^^^^^
+    // final formKey = new GlobalKey<FormState>(); //hoeft al niet - je hebt al een global key
+    // return Column( 2x Column achter elkaar - is niet nodig aangezien hij al een Parent heeft van Form of iets.
+    //   appBar: AppBar( //dit moet weg - je hebt al een AppBar met 'Event aanmaken'
+      //   title: Text('Dropdown Formfield Example'),
+      // ),
+      // children: [
+      //   Center(
+      //   child: Form( hoeft niet nog een form te maken wanneer de widget al in een form staat
+        //   key: _formKey,
+        //   child: Column(
+        //     mainAxisAlignment: MainAxisAlignment.start,
+        //     children: <Widget>[
+              return Container( //ik zou gewoon een container terug sturen (hij staat al in een Column) - dan behoud hij gewoon de vormgeving van de andere objecten. Kan je natuurlijk veranderen, dat is aan jou
                 padding: EdgeInsets.all(16),
                 child: DropDownFormField(
                   titleText: 'Mijn sport',
@@ -184,15 +179,12 @@ class MyCustomFormState extends State<MyCustomForm> {
                   textField: 'display',
                   valueField: 'value',
                 ),
-              ),
-              Container(
-                padding: EdgeInsets.all(16),
-                child: Text(_myActivityResult),
-              )
-            ],
-          ),
-        ),
-      ),
+              // ),
+            // ],
+          // ),
+        // ),
+      // ),
+      // ],
     );
   }
 }
