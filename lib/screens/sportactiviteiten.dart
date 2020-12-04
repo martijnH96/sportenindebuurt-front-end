b4faded8e89974f4b9509b11029568167270f39f
martijnH96 <martijnh1996@hotmail.com>
2020-12-04 16:02:32 +0100

    kan met een knop rijen toevoegen waarin je een timeframe in kan stellen.

--- lib/screens/sportactiviteiten.dart
+++ lib/screens/sportactiviteiten.dart
@@ -16,21 +16,7 @@ class sportactiviteitenState extends State<sportactiviteiten> {
   final _formKey = GlobalKey<FormState>();
 
   List _myActivities;
-
-
-  final int starttime = 0;
-  final int endtime = 1;
-  final int available = 2;
-  final int day = 3;
-
-  final ValueNotifier dayAndTimeSettings = ValueNotifier(0);
-
-  var timeSettings = [
-    "00:00",
-    "00:15",
-    false,
-    "Maandag"
-  ];
+  int dayAndTimeSettingsAmount = 1;
 
   Map mapped = Map<int, List>();
 
@@ -38,9 +24,12 @@ class sportactiviteitenState extends State<sportactiviteiten> {
   void initState() {
     super.initState();
     _myActivities = [];
-    for(int i = 1; i <= dayAndTimeSettings.value; i++){
-      mapped[i] = {i : timeSettings};
-    }
+  }
+
+  addTimeFrameRow(){
+    setState(() {
+      dayAndTimeSettingsAmount = dayAndTimeSettingsAmount + 1;
+    });
   }
 
   _saveForm() {
@@ -55,6 +44,7 @@ class sportactiviteitenState extends State<sportactiviteiten> {
   }
 
   Widget SportenForm(){
+    List<Widget> _timeFrames = new List.generate(dayAndTimeSettingsAmount, (index) => new timeFrameWidget());
     return Form(
         key: _formKey,
         child: Column(
@@ -125,17 +115,19 @@ class sportactiviteitenState extends State<sportactiviteiten> {
                 },
               ),
             ),
-            ValueListenableBuilder(
-              builder: (BuildContext context, dynamic value, Widget child){
-                return buildTimeFrame(value);
-              },
-              valueListenable: dayAndTimeSettings,
-            ),
+            new Container(
+                height: 400.0,
+                  padding: EdgeInsets.all(16),
+                  child: new ListView(
+                    children: _timeFrames,
+                  ),
+                ),
             RaisedButton(
-                onPressed: () => dayAndTimeSettings.value++,
+                onPressed: () {
+                  addTimeFrameRow();
+                },
               child: Text("Meer!"),
             ),
-            // buildTimeFrame(1),
             Builder(
                 builder: (BuildContext context){
                   return Container(
@@ -145,13 +137,13 @@ class sportactiviteitenState extends State<sportactiviteiten> {
                       height: 33,
                       child: RaisedButton(
                         onPressed: () async{
-                          Scaffold.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text(mapped.toString()),
-                              // duration: Duration(seconds: 1),
-                            ),
-                          );
-
+                          for(timeFrameWidget widget in _timeFrames){
+                            Scaffold.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text(widget.timeSettings.toString()),
+                              ),
+                            );
+                          }
                         },
                         child: Text("VOEG VOORKEUREN TOE"),
                       ),
@@ -164,102 +156,9 @@ class sportactiviteitenState extends State<sportactiviteiten> {
     );
   }
 
-
-  Widget buildTimeFrame(int i){
-    var days = [
-      "Maandag",
-      "Dinsdag",
-      "Woensdag",
-      "Donderdag",
-      "Vrijdag",
-      "Zaterdag",
-      "Zondag",
-    ];
-    var daytimeSettings = mapped.values.elementAt(i);
-    // var daytimeSettings = values[i];
-
-    List<String> startTimes = new List();
-    List<String> endTimes = new List();
-    for(int i = 00; i < 24; i++){
-      for(int j = 00; j < 4; j++){
-        startTimes.add((i.toString().length == 1 ? "0"+i.toString() : i.toString()) + ":" + ((j*15).toString().length == 1 ? j.toString()+"0" : (j*15).toString()));
-        endTimes.add((i.toString().length == 1 ? "0"+i.toString() : i.toString()) + ":" + ((j*15).toString().length == 1 ? j.toString()+"0" : (j*15).toString()));
-      }
-    }
-    return Container(
-      padding:
-      EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      margin: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(10)),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          DropdownButton<String>(
-              value: daytimeSettings[day],
-              icon: Icon(Icons.arrow_drop_down),
-              iconSize: 42,
-              underline: SizedBox(),
-              onChanged: (String newValue) {
-                setState(() {
-                  daytimeSettings[day] = newValue;
-                });
-              },
-              items: days.map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList()),
-          DropdownButton<String>(
-              value: daytimeSettings[starttime],
-              icon: Icon(Icons.arrow_drop_down),
-              iconSize: 42,
-              underline: SizedBox(),
-              onChanged: (String newValue) {
-                setState(() {
-                  daytimeSettings[starttime] = newValue;
-                });
-              },
-              items: startTimes.map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList()),
-          DropdownButton<String>(
-              value: daytimeSettings[endtime],
-              icon: Icon(Icons.arrow_drop_down),
-              iconSize: 42,
-              underline: SizedBox(),
-              onChanged: (String newValue) {
-                setState(() {
-                  daytimeSettings[endtime] = newValue;
-                });
-              },
-              items: endTimes.map((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList()),
-          // Checkbox(
-          //     value: daytimeSettings[available],
-          //     onChanged: (bool checked){
-          //       setState(() {
-          //         daytimeSettings[available] = checked;
-          //       });
-          //     }
-          // ),
-        ],
-      ),
-    );
-  }
-
   Widget buildDropDown() {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.end,
         children: <Widget>[
           SportenForm(),
         ],
@@ -283,6 +182,136 @@ class sportactiviteitenState extends State<sportactiviteiten> {
     );
   }
 }
+class timeFrameWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new timeFrameWidgetState();
+  var timeSettings = [
+    "00:00",
+    "00:15",
+    false,
+    "Maandag"
+  ];
+}
+
+class timeFrameWidgetState extends State<timeFrameWidget> {
+
+  void initState(){
+    for(int i = 00; i < 24; i++){
+      for(int j = 00; j < 4; j++){
+        startTimes.add((i.toString().length == 1 ? "0"+i.toString() : i.toString()) + ":" + ((j*15).toString().length == 1 ? j.toString()+"0" : (j*15).toString()));
+        endTimes.add((i.toString().length == 1 ? "0"+i.toString() : i.toString()) + ":" + ((j*15).toString().length == 1 ? j.toString()+"0" : (j*15).toString()));
+      }
+    }
+    timeSettings = widget.timeSettings;
+  }
+
+  static int dayAndTimeSettingsAmount = 1;
+  final ValueNotifier dayAndTimeSettings = ValueNotifier(0);
+
+  var timeSettings;
+
+
+  final int starttime = 0;
+  final int endtime = 1;
+  final int available = 2;
+  final int day = 3;
+
+
+  var days = [
+    "Maandag",
+    "Dinsdag",
+    "Woensdag",
+    "Donderdag",
+    "Vrijdag",
+    "Zaterdag",
+    "Zondag",
+  ];
+
+  List<String> startTimes = new List();
+  List<String> endTimes = new List();
+
+  Widget buildings() {
+    widget.timeSettings = timeSettings;
+    return Container(
+      padding:
+      EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      margin: EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          DropdownButton<String>(
+              value: timeSettings[day],
+              icon: Icon(Icons.arrow_drop_down),
+              iconSize: 42,
+              underline: SizedBox(),
+              onChanged: (String newValue) {
+                setState(() {
+                  timeSettings[day] = newValue;
+                });
+              },
+              items: days.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList()),
+          DropdownButton<String>(
+              value: timeSettings[starttime],
+              icon: Icon(Icons.arrow_drop_down),
+              iconSize: 42,
+              underline: SizedBox(),
+              onChanged: (String newValue) {
+                setState(() {
+                  timeSettings[starttime] = newValue;
+                });
+              },
+              items: startTimes.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList()),
+          DropdownButton<String>(
+              value: timeSettings[endtime],
+              icon: Icon(Icons.arrow_drop_down),
+              iconSize: 42,
+              underline: SizedBox(),
+              onChanged: (String newValue) {
+                setState(() {
+                  timeSettings[endtime] = newValue;
+                });
+              },
+              items: endTimes.map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList()),
+          Container(
+            height: 9,
+            width: 20,
+            child: Checkbox(
+                value: timeSettings[available],
+                onChanged: (bool checked){
+                  setState(() {
+                    timeSettings[available] = checked;
+                  });
+                }
+            ),
+          )
+        ],
+      ),
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return buildings();
+  }
+
+}
 
 // Completer<GoogleMapController> _controller = Completer();
 // static final CameraPosition _kHome = CameraPosition(
@@ -301,43 +330,3 @@ class sportactiviteitenState extends State<sportactiviteiten> {
 //   final GoogleMapController controller = await _controller.future;
 //   controller.animateCamera(CameraUpdate.newCameraPosition(_kSchool));
 // }
-
-// Expanded(
-//     child: ListView.builder
-//       (
-//         itemCount: _myActivities.length,
-//         itemBuilder: (BuildContext context, int i) {
-//           return new Container(
-//             child: Column(
-//               children: [
-//                 ListTile(
-//                   title: Text(_myActivities[i]),
-//                   // onTap: _pickDate,
-//                 ),
-//               Row(
-//                 mainAxisAlignment: MainAxisAlignment.end,
-//                 children: [
-//                   RaisedButton(
-//                       onPressed: (){
-//                         showDatePicker(
-//                             context: context,
-//                             initialDate: pickedDates[i],
-//                             firstDate: DateTime.now(),
-//                             lastDate: DateTime(DateTime.now().year+1)
-//                         ).then((value) {
-//                           setState(() {
-//                             pickedDates[i] = value;
-//                           });
-//                         });
-//                       }
-//                   ),
-//                 ],
-//               ),
-//               Text(pickedDates[i].toString())
-//               ],
-//             ),
-//
-//           );
-//         }
-//     )
-// ),
