820208f42c6339f959ad1556891c32a3bd2cf6b9
Colin <Colin.t.s@live.com>
2021-01-07 20:11:21 +0100

    Tests naar aparte file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,47 +1,5 @@
-import 'package:Sporten_in_de_buurt/materialsneeded.dart';
-import 'package:flutter_test/flutter_test.dart';
+
 
 
 void main() {
-  group('Remove', (){
-    test('remove from array when all atributes are false', () {
-      Map<String, bool> List = {
-        'Voetbal': false,
-        'Goals': false,
-        'Water': false,
-      };
-
-      RemoveFromList removeFromList = new RemoveFromList();
-      removeFromList.remove(List);
-
-      expect(List, {'Voetbal': false, 'Goals': false, 'Water': false});
-    });
-
-    test('remove from array when an atribute is true', () {
-      Map<String, bool> List = {
-        'Voetbal': false,
-        'Goals': true,
-        'Water': false,
-      };
-
-      RemoveFromList removeFromList = new RemoveFromList();
-      removeFromList.remove(List);
-
-      expect(List, {'Voetbal': false, 'Water': false});
-    });
-
-    test('remove from array when all atributes are true', () {
-      Map<String, bool> List = {
-        'Voetbal': true,
-        'Goals': true,
-        'Water': true,
-      };
-
-      RemoveFromList removeFromList = new RemoveFromList();
-      removeFromList.remove(List);
-
-      expect(List, {});
-    });
-  });
-
 }
